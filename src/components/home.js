537a47123c562c61a54268c3b661595a36701010
David Essien <davidessienshare@gmail.com>
2018-05-28 00:26:49 +0100

    call the banner component in home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 
+import Banner from './banner'
+
 class Home extends Component{
 	render(){
 		return(
-			<div>Home</div>
+			<div>
+				<Banner />
+			</div>
 		)
 	}
 }
