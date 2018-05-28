80e1ba6ae53cece249ecc6f8d8d76f63d878b173
David Essien <davidessienshare@gmail.com>
2018-05-28 01:46:59 +0100

    import Link from react-router-dom

--- src/components/artistsList.js
+++ src/components/artistsList.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import {Link} form 'react-router-dom'
 
 const ArtistsList = (props) =>{
-	console.log(props);
 
 	return(
-		<div>List</div>
+		<div>
+			
+		</div>
 	)
 }
 
