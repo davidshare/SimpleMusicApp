bc67dabb9ee04df2c38c9854c6b1cf23a11ca5ac
David Essien <davidessienshare@gmail.com>
2018-05-28 02:18:54 +0100

    import header component and call it

--- src/components/artist.js
+++ src/components/artist.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import Header from './header'
 
 class Artist extends Component{
 	render(){
 		return(
-			<div>Artist</div>
+			<div>
+				<Header/>
+			</div>
 		)
 	}
 }
