a68639faf973fcefd4460b6f3831893b44321d3a
David Essien <davidessienshare@gmail.com>
2018-05-28 01:03:26 +0100

    add constructor to home component

--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react'
 import Banner from './banner'
 
 class Home extends Component{
+
+	constructor(props){
+		super(props);
+		
+	}
+
 	render(){
 		return(
 			<div>
